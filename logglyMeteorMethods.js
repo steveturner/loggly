b33de74dfc0fd5d907bbd2211ff7ba32ea3b02d5
Steven Turner <sturner@ctic-inc.com>
2016-01-19 10:13:11 -0700

    update for security audit via checks.

--- logglyMeteorMethods.js
+++ logglyMeteorMethods.js
@@ -13,26 +13,32 @@ var loggerSet = function () {
 Meteor.methods({
   logglyLog: function(param, tag) {
     if (loggerSet()){
+      check(param, Match.Any);
+      check(tag, Match.Any);
       Logger.log(param, tag);
     }
   },
   logglyTrace: function(param, tag) {
     if (loggerSet()){
+      check(param, [Match.Any]);
       Logger.trace(param, tag);
     }
   },
   logglyInfo: function(param, tag) {
     if (loggerSet()){
+      check(param, [Match.Any]);
       Logger.info(param, tag);
     }
   },
   logglyWarn:  function(param, tag) {
     if (loggerSet()){
+      check(param, [Match.Any]);
       Logger.warn(param, tag);
     }
   },
   logglyError:  function(param, tag) {
     if (loggerSet()) {
+      check(param, [Match.Any]);
       Logger.error(param, tag);
     }
   }
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'miktam:loggly',
   summary: 'Loggly for Meteor',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/avrora/loggly/'
 });
 
